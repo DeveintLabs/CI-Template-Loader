8b1c62c534126894507d98db7001fff12b64805b
Nelson Ameyo <helixloft@gmail.com>
2015-02-02 00:05:21 -0800

    This is the initial commit

---
+++ template.php
@@ -0,0 +1,51 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Template
+{
+  protected 	$ci;
+
+  	# Header and footer location
+ 	public $header = 'inc/header';
+ 	public $footer = 'inc/footer';
+
+	public function __construct()
+	{
+        $this->ci =& get_instance();
+	}
+
+
+	public function load($views='', $data='')
+	{
+		// load header
+		if($this->header)
+		{
+			$this->ci->load->view($this->header, $data);
+			$data = '';
+		}
+
+		// Load content, can be more than one view
+		if(is_array($views))
+		{
+			foreach ($views as $view) 
+			{
+				$this->ci->load->view($view, $data);
+				$data = '';
+			}
+		} else {
+			$this->ci->load->view($views, $data);
+		}
+
+		// load footer
+		if($this->footer)
+		{
+			$this->ci->load->view($this->footer, $data);
+			$data = '';
+		}
+	}
+
+	
+
+}
+
+/* End of file MY_Template.php */
+/* Location: ./application/libraries/MY_Template.php */
